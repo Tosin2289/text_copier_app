20d42ba97df2d612c7ac40ae5a02a957d043ac7a
Tosin2289 <ayoolatosin00@gmail.com>
2023-02-25 20:50:35 +0100

    third commit

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -37,68 +37,70 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Stack(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 12.0, vertical: 12),
-                  child: TextField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    style: TextStyle(fontSize: 20),
-                    controller: _textcontroller,
-                    decoration: InputDecoration(
-                      hintStyle: TextStyle(fontSize: 20),
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 77, horizontal: 10),
-                      hintText: 'text',
-                      border: InputBorder.none,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.circular(12),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0, vertical: 12),
+                    child: TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      style: TextStyle(fontSize: 20),
+                      controller: _textcontroller,
+                      decoration: InputDecoration(
+                        hintStyle: TextStyle(fontSize: 20),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 77, horizontal: 10),
+                        hintText: 'text',
+                        border: InputBorder.none,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
                     ),
                   ),
-                ),
-                Positioned(
-                    top: 5,
-                    right: 10,
-                    child: Container(
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.copy_outlined,
-                          size: 30,
+                  Positioned(
+                      top: 5,
+                      right: 10,
+                      child: Container(
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.copy_outlined,
+                            size: 30,
+                          ),
+                          onPressed: copy,
                         ),
-                        onPressed: copy,
-                      ),
-                    )),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                padding: const EdgeInsets.all(12.0),
-                child: ElevatedButton(
-                  onPressed: copy,
-                  child: Text('Copy Text'),
-                ),
+                      )),
+                ],
               ),
-            )
-          ],
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  padding: const EdgeInsets.all(12.0),
+                  child: ElevatedButton(
+                    onPressed: copy,
+                    child: Text('Copy Text'),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
